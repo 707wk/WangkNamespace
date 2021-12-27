c34ca222035d253baead8ec487e2924351b71782
汪恳 <1174843091@qq.com>
2021-12-27 19:46:06 +0800

    添加 获取代码位置

--- Wangk.Hash/My Project/AssemblyInfo.vb
+++ Wangk.Hash/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.2111.31")>
+<Assembly: AssemblyFileVersion("1.0.2112.32")>
---
+++ Wangk.ResourceWPF/CodeHelper.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Class CodeHelper
+
+#Region "获取代码位置"
+    ''' <summary>
+    ''' 获取代码位置
+    ''' </summary>
+    Public Shared Function GetLocation(<CallerFilePath> Optional callerFilePath As String = Nothing,
+                                       <CallerLineNumber> Optional callerLineNumber As String = Nothing) As String
+
+        Return $"{IO.Path.GetFileNameWithoutExtension(callerFilePath)}-{callerLineNumber}"
+
+    End Function
+#End Region
+
+End Class
