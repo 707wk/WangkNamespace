1c0d468b8dc664657c12727a203e736647bc0d63
汪恳 <1174843091@qq.com>
2022-11-30 20:10:59 +0800

    调整 BackgroundWorkWindow 界面

--- Wangk.ResourceWPF/My Project/AssemblyInfo.vb
+++ Wangk.ResourceWPF/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 Imports System.Globalization
@@ -56,4 +56,4 @@ Imports System.Windows
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.2207.108")>
+<Assembly: AssemblyFileVersion("1.0.2211.110")>
