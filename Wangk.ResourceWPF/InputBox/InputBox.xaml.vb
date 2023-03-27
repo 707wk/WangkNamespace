3dcb1c9c82439f8e8fe21b6d5e7ebd32f8c962c9
汪恳 <1174843091@qq.com>
2023-03-27 15:59:56 +0800

    添加 InputBox, 移除 ConsoleDebug

---
+++ Wangk.ResourceWPF/InputBox/InputBox.xaml.vb
@@ -0,0 +1,42 @@
+﻿Public Class InputBox
+
+    Public Property ShowPrompt As String
+
+    Public Property ShowTitle As String
+
+    Public Property ShowText As String
+
+    Public Overloads Shared Function ShowDialog(prompt As String,
+                                                Optional title As String = "",
+                                                Optional text As String = "") As String
+
+        Dim tmpWindow As New InputBox With {
+            .ShowPrompt = prompt,
+            .ShowTitle = title,
+            .ShowText = text
+        }
+
+        If tmpWindow.ShowDialog() Then
+            Return tmpWindow.ShowText
+        Else
+            Return String.Empty
+        End If
+
+    End Function
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        DataContext = Me
+
+        WindowHelper.InitChildWindowStyle(Me)
+
+    End Sub
+
+    Private Sub Window_ContentRendered(sender As Object, e As EventArgs)
+        InputTextBox.SelectAll()
+    End Sub
+
+    Private Sub okButton_Click(sender As Object, e As RoutedEventArgs)
+        DialogResult = True
+    End Sub
+
+End Class
