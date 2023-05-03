54e15975a08b66dd4235b2465b2a74e57c26e5ed
汪恳 <1174843091@qq.com>
2023-05-04 00:20:30 +0800

    调整 Toast 样式

--- Wangk.Resource/My Project/AssemblyInfo.vb
+++ Wangk.Resource/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.2304.81")>
+<Assembly: AssemblyFileVersion("1.0.2305.82")>
