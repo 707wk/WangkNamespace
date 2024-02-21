86c47d8342d2e69868ce5e52596d4e37faf452b2
汪恳 <1174843091@qq.com>
2024-02-21 23:02:11 +0800

    添加 Object 转 Dictionary 功能

---
+++ Wangk.Hash/ObjectHelper.vb
@@ -0,0 +1,25 @@
+﻿Public Class ObjectHelper
+
+    Public Shared Function ToDic(obj As Object) As Dictionary(Of String, String)
+        Dim tmpMap = New Dictionary(Of String, String)
+
+        If obj Is Nothing Then Return tmpMap
+
+        Dim t = obj.GetType
+        Dim propertyList = t.GetProperties
+
+        For Each item In propertyList
+
+            Dim key = item.Name
+            Dim value As Object = item.GetValue(obj)
+
+            If value IsNot Nothing Then
+                tmpMap.Add(key, value)
+            End If
+
+        Next
+
+        Return tmpMap
+    End Function
+
+End Class
