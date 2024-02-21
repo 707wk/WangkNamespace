ea02caed7fac056c698c868381bbdf8c4e58c1bf
汪恳 <1174843091@qq.com>
2024-02-21 23:33:58 +0800

    移除 Object 转 Dictionary 功能

--- Wangk.ResourceWPF/My Project/AssemblyInfo.vb
+++ Wangk.ResourceWPF/My Project/AssemblyInfo.vb
@@ -56,4 +56,4 @@ Imports System.Windows
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.2402.234")>
+<Assembly: AssemblyFileVersion("1.0.2402.235")>
