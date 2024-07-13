1f5ff9917a21da9569f5afc8a0d3e4e281f1c402
汪恳 <1174843091@qq.com>
2024-07-13 17:29:31 +0800

    更新 EPPlus 版本

--- DownloadUpdate/My Project/AssemblyInfo.vb
+++ DownloadUpdate/My Project/AssemblyInfo.vb
@@ -56,4 +56,4 @@ Imports System.Windows
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.2406.136")>
+<Assembly: AssemblyFileVersion("1.0.2407.137")>
