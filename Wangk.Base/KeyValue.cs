ac7d22ea415eed23ded2cdbfe34c700d8a04d0bb
汪恳 <1174843091@qq.com>
2024-09-04 21:57:00 +0800

    Base 基础类添加 KeyValue / SystemEventLog 类

---
+++ Wangk.Base/KeyValue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangk.Base
+{
+    /// <summary>
+    /// 键值对, struct 类型使用系统自带的 KeyValuePair
+    /// </summary>
+    public class KeyValue<TKey, TValue>
+    {
+        /// <summary>
+        /// 无参构造
+        /// </summary>
+        public KeyValue()
+        {
+        }
+
+        /// <summary>
+        /// 带参构造
+        /// </summary>
+        public KeyValue(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        /// <summary>
+        /// 键
+        /// </summary>
+        public TKey Key { get; set; }
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public TValue Value { get; set; }
+    }
+}
---
+++ Wangk.Base/SystemEventLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangk.Base
+{
+    /// <summary>
+    /// 系统日志信息
+    /// </summary>
+    public class SystemEventLog
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 系统名称
+        /// </summary>
+        public string SystemName { get; set; }
+
+        /// <summary>
+        /// 事件名称
+        /// </summary>
+        public string EventName { get; set; }
+
+        /// <summary>
+        /// 事件明细
+        /// </summary>
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// 设备 Id
+        /// </summary>
+        public string DeviceId { get; set; }
+
+    }
+}
