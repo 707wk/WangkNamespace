e1a1de17337747ca2941e8c0e6c41470b71553cf
汪恳 <1174843091@qq.com>
2024-09-04 19:15:48 +0800

    添加 Base 基础类

---
+++ Wangk.Base/ObjectEditType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangk.Base
+{
+    /// <summary>
+    /// 对象编辑操作
+    /// </summary>
+    public enum ObjectEditType
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        Add,
+        /// <summary>
+        /// 删除
+        /// </summary>
+        Delete,
+        /// <summary>
+        /// 替换
+        /// </summary>
+        Replace,
+        /// <summary>
+        /// 更新
+        /// </summary>
+        Update
+    }
+}
---
+++ Wangk.Base/ResultMsg.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangk.Base
+{
+    /// <summary>
+    /// 请求结果
+    /// </summary>
+    public class ResultMsg<T>
+    {
+
+        /// <summary>
+        /// 执行成功
+        /// </summary>
+        public static int SUCCESSCode = 200;
+
+        /// <summary>
+        /// 执行失败
+        /// </summary>
+        public static int FAILCode = 404;
+
+        /// <summary>
+        /// 请求处理状态
+        /// 200: 请求处理成功
+        /// 404: 请求处理失败
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// 请求处理是否成功
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// 请求处理消息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 返回的实体数据
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// 总记录数, 用于分页查询
+        /// </summary>
+        public int? TotalCount { get; set; }
+
+        /// <summary>
+        /// 无参构造, 用于序列化
+        /// </summary>
+        public ResultMsg()
+        {
+        }
+
+        /// <summary>
+        /// 执行成功
+        /// </summary>
+        /// <param name="value">返回的结果</param>
+        public ResultMsg(T value)
+        {
+            Code = SUCCESSCode;
+            Success = true;
+
+            Data = value;
+        }
+
+        /// <summary>
+        /// 执行成功
+        /// </summary>
+        /// <param name="value">返回的结果</param>
+        /// <param name="count">总记录数</param>
+        public ResultMsg(T value, int count)
+        {
+            Code = SUCCESSCode;
+            Success = true;
+
+            Data = value;
+
+            TotalCount = count;
+        }
+
+        /// <summary>
+        /// 执行失败
+        /// </summary>
+        public ResultMsg(Exception e)
+        {
+            Code = FAILCode;
+            Success = false;
+
+            Message = e.Message;
+        }
+
+        /// <summary>
+        /// 自动包装返回结果
+        /// </summary>
+        public static implicit operator ResultMsg<T>(T value)
+        {
+            return new ResultMsg<T>(value);
+        }
+
+    }
+}
