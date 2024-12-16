ec6eb8f4db80afc344541c4d7d53082066dd159a
汪恳 <1174843091@qq.com>
2024-12-16 13:48:30 +0800

    添加 基础搜索值类

---
+++ Wangk.Base/BaseSearchValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangk.Base
+{
+    /// <summary>
+    /// 基础搜索值信息
+    /// </summary>
+    public class BaseSearchValue
+    {
+        /// <summary>
+        /// 关键字
+        /// </summary>
+        public string Keyword {  get; set; }
+    }
+}
