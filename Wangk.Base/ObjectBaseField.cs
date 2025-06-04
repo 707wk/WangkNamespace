ec8cc4def93b9e2c802de153e357ea430129b1e3
汪恳 <1174843091@qq.com>
2025-06-04 17:52:03 +0800

    新增 对象基本字段 基类

---
+++ Wangk.Base/ObjectBaseField.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangk.Base
+{
+    /// <summary>
+    /// 对象基本字段
+    /// </summary>
+    public class ObjectBaseField
+    {
+        /// <summary>
+        /// 数据主键
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime UpdatedAt { get; set; }
+
+        /// <summary>
+        /// 更新人
+        /// </summary>
+        public string UpdatedBy { get; set; }
+    }
+}
