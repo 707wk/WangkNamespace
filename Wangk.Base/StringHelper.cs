a9496d98a2f0686bdf22b630091934aeab51078a
汪恳 <1174843091@qq.com>
2025-06-12 19:23:08 +0800

    优化 Markdown 格式转换

--- Wangk.Base/StringHelper.cs
+++ Wangk.Base/StringHelper.cs
@@ -38,13 +38,81 @@ namespace Wangk.Base
         }
         #endregion
 
-        #region 保留 Markdown 的转义字符
+        #region 转义 Markdown 特殊字符
         /// <summary>
-        /// 保留 Markdown 的转义字符
+        /// Markdown需要转义的特殊字符
         /// </summary>
-        public static string ToMarkdownText(this string text)
+        private static readonly char[] _markdownSpecialChars =
+            { '\\', '`', '*', '_', '{', '}', '[', ']', '(', ')',
+          '#', '+', '-', '.', '!', '|', '~', '>', '<' };
+
+        /// <summary>
+        /// HTML特殊字符及其对应实体
+        /// </summary>
+        private static readonly (string Char, string Entity)[] _htmlSpecialChars =
         {
-            return text.Replace("*", "\\*");
+        ("&", "&amp;"),
+        ("<", "&lt;"),
+        (">", "&gt;"),
+        ("\"", "&quot;"),
+        ("'", "&apos;"),
+        ("©", "&copy;"),
+        ("®", "&reg;"),
+        ("™", "&trade;"),
+        (" ", "&nbsp;")
+    };
+
+        /// <summary>
+        /// 转义字符串中的Markdown特殊字符和HTML特殊字符
+        /// </summary>
+        /// <param name="input">要转义的字符串</param>
+        /// <param name="escapeHtml">是否转义HTML特殊字符</param>
+        /// <returns>转义后的字符串</returns>
+        public static string EscapeMarkdown(this string input, bool escapeHtml = true)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            var sb = new StringBuilder(input.Length * 2);
+
+            foreach (char c in input)
+            {
+                // 转义Markdown特殊字符
+                if (Array.IndexOf(_markdownSpecialChars, c) >= 0)
+                {
+                    sb.Append('\\');
+                }
+
+                // 转义HTML特殊字符
+                if (escapeHtml)
+                {
+                    var entity = GetHtmlEntity(c.ToString());
+                    sb.Append(entity ?? c.ToString());
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 将HTML特殊字符转换为实体
+        /// </summary>
+        private static string GetHtmlEntity(string character)
+        {
+            foreach (var (Char, Entity) in _htmlSpecialChars)
+            {
+                if (Char == character)
+                {
+                    return Entity;
+                }
+            }
+            return null;
         }
         #endregion
 
