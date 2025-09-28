f877ab951bc3bbf12c74937ddbcf455a5c991d8b
汪恳 <1174843091@qq.com>
2025-09-28 10:32:52 +0800

    添加 PostData 类型, 用于钉钉调用外部接口时的单个参数封装

---
+++ Wangk.Base/PostData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangk.Base
+{
+    /// <summary>
+    /// 提交的数据, 用于钉钉调用外部接口时的单个参数封装
+    /// </summary>
+    public class PostData<T>
+    {
+        /// <summary>
+        /// 提交的实体数据
+        /// </summary>
+        public T Data { get; set; }
+    }
+}
