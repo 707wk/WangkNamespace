4f156d5ea12d9ea9029002aaccc5a7f9390738df
汪恳 <1174843091@qq.com>
2025-10-25 03:06:59 +0800

    ResultMsg 添加非泛型版本

--- Wangk.Base/ResultMsg.cs
+++ Wangk.Base/ResultMsg.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Wangk.Base
 {
     /// <summary>
-    /// 请求结果
+    /// 请求结果, 泛型版本
     /// </summary>
     public class ResultMsg<T>
     {
@@ -89,4 +89,70 @@ namespace Wangk.Base
         }
 
     }
+
+    /// <summary>
+    /// 请求结果, 非泛型版本
+    /// </summary>
+    public class ResultMsg
+    {
+
+        /// <summary>
+        /// 请求处理是否成功
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// 请求处理失败时的消息提示
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 无参构造, 用于序列化
+        /// </summary>
+        public ResultMsg()
+        {
+        }
+
+        /// <summary>
+        /// 执行成功
+        /// </summary>
+        /// <param name="value">语法要求必须返回一个明确的值, 只能传入 true</param>
+        public ResultMsg(bool value)
+        {
+            if (!value)
+            {
+                throw new ArgumentException("必须传入 true 以表示执行成功");
+            }
+
+            Success = true;
+        }
+
+        /// <summary>
+        /// 执行失败
+        /// </summary>
+        public ResultMsg(Exception e)
+        {
+            Success = false;
+
+            Message = e.Message;
+        }
+
+        /// <summary>
+        /// 自动包装返回结果, 语法要求必须返回一个明确的值, 只能传入 true
+        /// </summary>
+        public static implicit operator ResultMsg(bool value)
+        {
+            return new ResultMsg(value);
+        }
+
+        /// <summary>
+        /// 自动包装异常返回结果
+        /// </summary>
+        public static implicit operator ResultMsg(Exception e)
+        {
+            return new ResultMsg(e);
+        }
+
+    }
+
 }
