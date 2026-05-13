715cd138b6a502fb5d5a1eff5d5d28f2ea082442
汪恳 <1174843091@qq.com>
2026-05-14 07:34:47 +0800

    新增支持多语言的业务异常

---
+++ Wangk.Base/BusinessException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wangk.Base
+{
+    /// <summary>
+    /// 业务异常
+    /// </summary>
+    public class BusinessException : Exception
+    {
+        /// <summary>
+        /// 资源的键名
+        /// </summary>
+        public string ErrorKey { get; }
+        /// <summary>
+        /// 格式化参数
+        /// </summary>
+        public Dictionary<string, object> Parameters { get; }
+
+        /// <summary>
+        /// 多个参数的构造函数
+        /// </summary>
+        public BusinessException(string errorKey, Dictionary<string, object> parameters = null)
+        : base(errorKey)
+        {
+            ErrorKey = errorKey;
+            Parameters = parameters ?? new Dictionary<string, object>();
+        }
+
+        /// <summary>
+        /// 单个参数的构造函数
+        /// </summary>
+        public BusinessException(string errorKey, string paramName, object paramValue)
+            : this(errorKey, new Dictionary<string, object> { { paramName, paramValue } })
+        {
+        }
+    }
+}
